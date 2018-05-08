2964329dbfaef51df3a096ac963c36e73ad8a489
Guilherme Lins <Lins.guilherme11@gmail.com>
2018-05-08 10:31:07 -0300

    modificação do método de identificação de letra minúscula

--- src/Util.java
+++ src/Util.java
@@ -1,6 +1,6 @@
 public class Util {
     public boolean isLowerCase(char chr){
-        if (chr == 'a'|| chr == 'b'|| chr == 'c'|| chr == 'd'|| chr == 'e'|| chr == 'f'|| chr == 'g'|| chr == 'h'|| chr == 'i'|| chr == 'j'|| chr == 'k'|| chr == 'l'|| chr == 'm'|| chr == 'n'|| chr == 'o'|| chr == 'p'|| chr == 'q'|| chr == 'r'|| chr == 's'|| chr == 't'|| chr == 'u'|| chr == 'v'|| chr == 'w'|| chr == 'x'|| chr == 'y'|| chr == 'z')
+        if (chr == Character.toLowerCase(chr))
             return true;
     }
 }
